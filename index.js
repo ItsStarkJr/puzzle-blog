773129609a5d4878174d63d5e26b7e907f4d039e
ItsStarkJr <97918042+ItsStarkJr@users.noreply.github.com>
2024-08-13 09:03:22 +0200

    Routing testing...

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-document.querySelectorAll(".post").forEach((post) => {
-  post.addEventListener("click", () => {
-    const postID = post.id;
-    window.open(`/posts/${postID}/`, "_blank");
-  });
-});
+// document.querySelectorAll(".post").forEach((post) => {
+//   post.addEventListener("click", () => {
+//     const postID = post.id;
+//     window.open(`/posts/${postID}/`, "_blank");
+//   });
+// });
 
 document.querySelectorAll("#share-button").forEach((button) => {
   button.addEventListener("click", () => {
