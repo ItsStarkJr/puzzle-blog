7050b0e22f1b1c3560e71c62f48b25831176c7b9
ItsStarkJr <97918042+ItsStarkJr@users.noreply.github.com>
2024-08-13 04:43:54 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+document.querySelectorAll("#share-button").forEach((button) => {
+  button.addEventListener("click", () => {
+    const title = button.previousElementSibling.previousElementSibling;
+    const bob = title.textContent;
+    alert(bob);
+  });
+});
+
+document.querySelectorAll(".post").forEach((post) => {
+  post.addEventListener("click", () => {
+    const postID = post.id;
+    window.open(`/posts/${postID}.html`, "_blank");
+  });
+});
---
+++ js/post.js
@@ -0,0 +1,15 @@
+const shareButton = document.getElementById("share-button");
+shareButton.addEventListener("click", () => {
+  const currentUrl = window.location.href;
+  const tooltip = document.getElementById("copied-tip");
+  navigator.clipboard.writeText(currentUrl);
+  tooltip.classList.remove("hidden");
+  setTimeout(() => {
+    tooltip.classList.add("hidden");
+  }, 1500);
+});
+
+const homeButton = document.getElementById("home-button");
+homeButton.addEventListener("click", () => {
+  window.location.href = "../index.html";
+});
