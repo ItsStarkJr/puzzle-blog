e0debc1c7a02c0876735f6d52ee3c0d633661cbb
ItsStarkJr <97918042+ItsStarkJr@users.noreply.github.com>
2024-08-15 21:49:32 +0200

    Small things

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-import shutil
+# import shutil
 
-shutil.copyfile(
-    "C:/Users/Wessel/Pictures/Screenshots/123234234.png", "assets/images/2.png"
-)
+# shutil.copyfile(
+#     "C:/Users/Wessel/Pictures/Screenshots/123234234.png", "assets/images/2.png"
+# )
+
+import webbrowser
+
+webbrowser.open_new_tab("create-post.html")
