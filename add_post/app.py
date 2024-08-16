b60e2f26efff440867720f388f71fa3dfcb455c3
ItsStarkJr <97918042+ItsStarkJr@users.noreply.github.com>
2024-08-17 00:41:10 +0200

    Finished getting data via flask app

--- add_post/app.py
+++ add_post/app.py
@@ -1,53 +1,66 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
 import webbrowser
 import json
 
 app = Flask(__name__)
+app.secret_key = "bobbelientjuh69420"
 
 
 @app.route("/", methods=["POST", "GET"])
 def submit():
     if request.method == "POST":
         # Retrieve the form data
-        data = {}
-        data["difficulty"] = request.form.get("difficulty")
-        data["variant"] = request.form.get("variant")
-        data["genre"] = request.form.get("genre")
-        data["title"] = request.form.get("title")
-        data["intro"] = request.form.get("intro")
-        data["rules"] = request.form.get("rules")
+        data = {
+            "difficulty": request.form.get("difficulty"),
+            "variant": request.form.get("variant"),
+            "genre": request.form.get("genre"),
+            "title": request.form.get("title"),
+            "intro": request.form.get("intro"),
+            "rules": request.form.get("rules"),
+            "tags": [
+                tag
+                for tag in [
+                    "drawing",
+                    "hybrid",
+                    "loop",
+                    "number-placement",
+                    "object-placement",
+                    "pack",
+                    "path",
+                    "region-division",
+                    "shading",
+                ]
+                if request.form.get(tag)
+            ],
+            "links": {
+                link: request.form.get(link)
+                for link in ["puzz.link", "sudokupad", "penpa"]
+                if request.form.get(link)
+            },
+        }
 
-        # Retrieve checkbox values
-        data["tags"] = []
-        if request.form.get("drawing"):
-            data["tags"].append("drawing")
-        if request.form.get("hybrid"):
-            data["tags"].append("hybrid")
-        if request.form.get("loop"):
-            data["tags"].append("loop")
-        if request.form.get("number-placement"):
-            data["tags"].append("number-placement")
-        if request.form.get("object-placement"):
-            data["tags"].append("object-placement")
-        if request.form.get("pack"):
-            data["tags"].append("pack")
-        if request.form.get("path"):
-            data["tags"].append("path")
-        if request.form.get("region-division"):
-            data["tags"].append("region-division")
-        if request.form.get("shading"):
-            data["tags"].append("shading")
+        # Store the data dictionary in the session
+        session["data"] = data
 
-        with open("data.json", "w") as file:
-            json.dump(data, file)
-            print(f"Saved to data.json")
+        # with open("data.json", "w") as file:
+        #     json.dump(data, file)
+        #     print(f"Saved to data.json")
+        print(data)
 
-        return "<p>success</p>"
+        # return "<p>success</p>"
+        return render_template("index.html", data=data)
 
     else:
-        return render_template("index.html")
+        data = session.get("data", {})
+        return render_template("index.html", data=data)
+
+
+@app.route("/preview")
+def preview():
+    data = session.get("data", {})
+    return render_template("preview.html", data=data)
 
 
 if __name__ == "__main__":
-    webbrowser.open_new("http://127.0.0.1:5000")
+    # webbrowser.open_new("http://127.0.0.1:5000")
     app.run(debug=True)
